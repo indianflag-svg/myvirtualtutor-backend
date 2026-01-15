3b3144fc95a598db9eab7eb5bb7523035f48a3cd
Barath Rajaratnam <indianflag@mac.myfiosgateway.com>
2026-01-15 00:27:24 -0500

    Force OPTIONS preflight to return 204

--- server.js
+++ server.js
@@ -4,7 +4,21 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-app.set('trust proxy', 1);
+app.set("trust proxy", 1);
+
+// Hard-stop preflight early so it can never throw
+app.options("*", (req, res) => {
+  const origin = req.headers.origin;
+  if (origin) {
+    res.setHeader("Vary", "Origin");
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Max-Age", "86400");
+  }
+  return res.sendStatus(204);
+});
 
 const PORT = process.env.PORT || 10000;
 
