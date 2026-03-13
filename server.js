5f98e6380bbefa42bf86b5cd49030c40bf9ba03a
Ethan Barath <ethanbarath01@gmail.com>
2026-03-13 18:05:39 -0400

    prepare backend for openai tutor

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import OpenAI from "openai"
 
 const app = express()
 
@@ -10,6 +11,10 @@ app.use(cors({
 }))
 
 app.use(express.json())
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+})
 
 app.get("/", (req,res)=>{
   res.send("MyVirtualTutor backend running")
