008505186fe04b30b3ee0a2743e7f49e0bcb2ed3
Ethan Barath <ethanbarath01@gmail.com>
2026-03-23 23:43:17 -0400

    fix double response bug

--- server.render.cjs
+++ server.render.cjs
@@ -8,9 +8,6 @@ app.use(express.json())
 const PORT = process.env.PORT || 8000
 
 app.post("/chat", async (req, res) => {
-  const userMessage = req.body.message
-
-  // HARD TEST RESPONSE
   res.json({
     reply: "TEST WORKING\n\nDoes that make sense, or do you want me to explain any step?"
   })
